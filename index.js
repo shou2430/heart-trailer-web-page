4d05fc171adb566775d5b124488cfed3a144efd3
Jason Liu <j84yes116@gmail.com>
2022-12-04 00:51:40 +0800

    use touch event

--- index.js
+++ index.js
@@ -2,54 +2,8 @@ const bodyE1 = document.querySelector("body");
 
 bodyE1.addEventListener("mousemove", function(heartEvent) {animate(heartEvent)});
 // let the effect works on phone
-document.addEventListener('touchstart', handleTouchStart, false);        
-document.addEventListener('touchmove', handleTouchMove, false);
-
-var xDown = null;                                                        
-var yDown = null;
-
-function getTouches(evt) {
-  return evt.touches ||             // browser API
-         evt.originalEvent.touches; // jQuery
-}                                                     
-                                                                         
-function handleTouchStart(evt) {
-    const firstTouch = getTouches(evt)[0];                                      
-    xDown = firstTouch.clientX;                                      
-    yDown = firstTouch.clientY;                                      
-};                                                
-                                                                         
-function handleTouchMove(evt) {
-    if ( ! xDown || ! yDown ) {
-        return;
-    }
-
-    var xUp = evt.touches[0].clientX;                                    
-    var yUp = evt.touches[0].clientY;
-
-    var xDiff = xDown - xUp;
-    var yDiff = yDown - yUp;
-                                                                         
-    // if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) 
-    // {/*most significant*/
-    //     if ( xDiff > 0 ) {
-    //         /* right swipe */ 
-    //     } else {
-    //         /* left swipe */
-    //     }                       
-    // } else {
-    //     if ( yDiff > 0 ) {
-    //         /* down swipe */ 
-    //     } else { 
-    //         /* up swipe */
-    //     }                                                                 
-    // }
-    animate(evt);
-    /* reset values */
-    xDown = null;
-    yDown = null;                                             
-};
-
+document.addEventListener('touchstart', function(heartEvent) {animate(heartEvent)});        
+document.addEventListener('touchmove', function(heartEvent) {animate(heartEvent)});
 
 
 function animate(event){
